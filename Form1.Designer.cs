17d0ca8692df2d331c3e5f8eda199f15fbe887a4
elifb <elifb@DESKTOP-R9ITCEQ>
2022-03-27 22:20:05 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Ders31LinqSorgularRestriction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.radioButton11 = new System.Windows.Forms.RadioButton();
+            this.radioButton12 = new System.Windows.Forms.RadioButton();
+            this.radioButton13 = new System.Windows.Forms.RadioButton();
+            this.radioButton14 = new System.Windows.Forms.RadioButton();
+            this.radioButton15 = new System.Windows.Forms.RadioButton();
+            this.radioButton16 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(798, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Linq Entity";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 167);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(161, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.Text = "1.Sınavı 50den Küçük Notlar";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(24, 190);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(91, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Adı Elif olanlar\r\n";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(582, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Adı ve Soyadı";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(660, 166);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyad.TabIndex = 4;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.radioButton3.Location = new System.Drawing.Point(24, 213);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(137, 30);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.Text = "Ad ve soyadı Textten Al\r\n\r\n";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(24, 236);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(108, 17);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.Text = "Öğrenci Soyadları";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(24, 259);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(196, 17);
+            this.radioButton5.TabIndex = 9;
+            this.radioButton5.Text = "Ad Büyük Soyad Küçük Şartlı Seçim\r\n";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(290, 171);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(76, 17);
+            this.radioButton6.TabIndex = 10;
+            this.radioButton6.Text = "Geçti Kaldı\r\n";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(290, 194);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(85, 17);
+            this.radioButton7.TabIndex = 11;
+            this.radioButton7.Text = "Geçti Kaldı 2";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(290, 217);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(85, 17);
+            this.radioButton8.TabIndex = 12;
+            this.radioButton8.Text = "Geçti Kaldı 3";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(434, 166);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(77, 17);
+            this.radioButton9.TabIndex = 13;
+            this.radioButton9.Text = "İlk 3 Değer";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(434, 190);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(85, 17);
+            this.radioButton10.TabIndex = 14;
+            this.radioButton10.Text = "Son 3 Değer";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // radioButton11
+            // 
+            this.radioButton11.AutoSize = true;
+            this.radioButton11.Location = new System.Drawing.Point(434, 217);
+            this.radioButton11.Name = "radioButton11";
+            this.radioButton11.Size = new System.Drawing.Size(99, 17);
+            this.radioButton11.TabIndex = 15;
+            this.radioButton11.Text = "Ada Göre Sırala";
+            this.radioButton11.UseVisualStyleBackColor = true;
+            // 
+            // radioButton12
+            // 
+            this.radioButton12.AutoSize = true;
+            this.radioButton12.Location = new System.Drawing.Point(434, 240);
+            this.radioButton12.Name = "radioButton12";
+            this.radioButton12.Size = new System.Drawing.Size(100, 17);
+            this.radioButton12.TabIndex = 16;
+            this.radioButton12.Text = "İlk 5 Değeri Atla";
+            this.radioButton12.UseVisualStyleBackColor = true;
+            // 
+            // radioButton13
+            // 
+            this.radioButton13.AutoSize = true;
+            this.radioButton13.Location = new System.Drawing.Point(434, 263);
+            this.radioButton13.Name = "radioButton13";
+            this.radioButton13.Size = new System.Drawing.Size(56, 17);
+            this.radioButton13.TabIndex = 17;
+            this.radioButton13.Text = "Grupla";
+            this.radioButton13.UseVisualStyleBackColor = true;
+            // 
+            // radioButton14
+            // 
+            this.radioButton14.AutoSize = true;
+            this.radioButton14.Location = new System.Drawing.Point(434, 286);
+            this.radioButton14.Name = "radioButton14";
+            this.radioButton14.Size = new System.Drawing.Size(62, 17);
+            this.radioButton14.TabIndex = 18;
+            this.radioButton14.Text = "Grupla2";
+            this.radioButton14.UseVisualStyleBackColor = true;
+            // 
+            // radioButton15
+            // 
+            this.radioButton15.AutoSize = true;
+            this.radioButton15.Location = new System.Drawing.Point(585, 213);
+            this.radioButton15.Name = "radioButton15";
+            this.radioButton15.Size = new System.Drawing.Size(77, 17);
+            this.radioButton15.TabIndex = 19;
+            this.radioButton15.Text = "MAXIMUM";
+            this.radioButton15.UseVisualStyleBackColor = true;
+            // 
+            // radioButton16
+            // 
+            this.radioButton16.AutoSize = true;
+            this.radioButton16.Location = new System.Drawing.Point(585, 236);
+            this.radioButton16.Name = "radioButton16";
+            this.radioButton16.Size = new System.Drawing.Size(74, 17);
+            this.radioButton16.TabIndex = 20;
+            this.radioButton16.Text = "MINIMUM";
+            this.radioButton16.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(691, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "......";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton16);
+            this.Controls.Add(this.radioButton15);
+            this.Controls.Add(this.radioButton14);
+            this.Controls.Add(this.radioButton13);
+            this.Controls.Add(this.radioButton12);
+            this.Controls.Add(this.radioButton11);
+            this.Controls.Add(this.radioButton10);
+            this.Controls.Add(this.radioButton9);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.RadioButton radioButton11;
+        private System.Windows.Forms.RadioButton radioButton12;
+        private System.Windows.Forms.RadioButton radioButton13;
+        private System.Windows.Forms.RadioButton radioButton14;
+        private System.Windows.Forms.RadioButton radioButton15;
+        private System.Windows.Forms.RadioButton radioButton16;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders31LinqSorgularRestriction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DbOkulEntities db = new DbOkulEntities();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Görünmesi İstenenler
+            if (radioButton1.Checked == true)
+            {
+                Checked50denKucuk();
+                //------------------------
+                //var degerler = db.Tbl_Notlar.Where(x => x.Sinav1 < 50);
+                //dataGridView1.DataSource = degerler.ToList();
+                //Görünmesin1 
+                //dataGridView1.Columns[9].Visible = false;
+                //--------------------------------------------------
+            }
+            //--
+            if (radioButton2.Checked == true)
+            {
+                AdiElifOlanlariGetir();
+
+            }
+            if (radioButton3.Checked == true)
+            {
+                AdiSoyadiTexttenAl();
+
+            }
+            //Linq Sorgular Select Projection (Yansıtma) Örnekleri
+            if (radioButton4.Checked == true)
+            {
+                OgrenciSoyadiGetir();
+
+            }
+            if (radioButton5.Checked == true)
+            {
+                //Adı büyük soyadı küçük ve Ad Elif olmayanları Listele
+                AdBuyukSoyadKucukSartliSecim();
+            }
+            // Linq Select Metodu Örnekleri
+            if (radioButton6.Checked == true)
+            {
+                // SQL DE ORTALAMA VE DURUM İÇİN SORGULAR 
+                /* --Update Tbl_Notlar set Ortalama=(Sinav1+Sinav2+Sinav3)/3; ==>> OrtalamaBul
+                 
+                 * -- DURUM ;
+                 * --Update Tbl_Notlar set Durum=0 where Ortalama<50; -- Ortalama<50 olanlar false
+                 * --update Tbl_Notlar set Durum=1 where Ortalama>=50;  --Ortalama>=50 olanlar true
+                 */
+                GectiKaldi();
+
+            }
+            if (radioButton7.Checked == true)
+            {
+                //TABLO BİRLEŞTİRME YAPILMALI 
+                var query = db.Tbl_Notlar.Select(x =>
+                  new
+                  {
+                      ÖĞRENCİID = x.OgrId,
+                      ORTALAMA = x.Ortalama,
+                      Durum = x.Durum == true ? "Geçti" : "Kaldı"
+
+                  });
+                dataGridView1.DataSource = query.ToList();
+            }
+            // 34.SelectMany(Projection) Kullanımı
+            if (radioButton8.Checked == true)
+            {
+                GectiKaldi3();
+            }
+            if (radioButton9.Checked == true)
+            {
+                //Ders 35 Bölümleme Take Skip Then Kullanımları
+                //-- TAKE -- Id ye gore sırala ilk 3 değeri al 
+                Ilk3OgrenciyiAl();
+            }
+            if (radioButton10.Checked == true)
+            {
+                //-- TAKE -- Id ye gore sırala son 3 değeri al 
+                Son3Ogrenci();
+
+            }
+            if (radioButton11.Checked == true)
+            {
+                //Ada göre sırala
+                AdaGoreSirala();
+
+            }
+            if (radioButton12.Checked == true)
+            {
+                //İlk 5 degeri atla 
+                Ilk5DegeriAtla();
+
+            }
+            if (radioButton13.Checked == true)
+            {
+                //36 Linq Sorgularda Group By Kullanımı ~ 1
+                GroupBy1();
+            }
+            if (radioButton14.Checked==true)
+            {
+                GroupBy2();
+            }
+
+            if (radioButton15.Checked == true)
+            {
+                //Maximum ortalama
+                label2.Text = db.Tbl_Notlar.Max(q => q.Ortalama).ToString();
+
+            }
+            if (radioButton16.Checked == true)
+            {
+                //minimum sınav 1 puanı
+                label2.Text = db.Tbl_Notlar.Min(q => q.Sinav1).ToString();
+
+            }
+
+        }
+
+        public void GroupBy2()
+        {
+            var query2 = from q1 in db.Tbl_Ogr
+                         orderby q1.Sehir
+                         group q1 by q1.Sehir into g
+                         select new
+                         {
+                             Şehirler = g.Key,
+                             Toplam = g.Count()
+                         };
+            dataGridView1.DataSource = query2.ToList();
+        }
+
+        public void GroupBy1()
+        {
+            var query = db.Tbl_Ogr.OrderBy(q => q.Sehir).GroupBy(q => q.Sehir).Select
+                (x => new
+                {
+                    Şehirler = x.Key,
+                    Toplam = x.Count()
+                });
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        public void Ilk5DegeriAtla()
+        {
+            var query = db.Tbl_Ogr.OrderBy(x => x.Ogr_Id).Skip(5);
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        public void AdaGoreSirala()
+        {
+            var query = db.Tbl_Ogr.OrderBy(x => x.Ogr_Ad);
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        public void Son3Ogrenci()
+        {
+            var query = db.Tbl_Ogr.OrderByDescending(x => x.Ogr_Id).Take(3);
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        public void Ilk3OgrenciyiAl()
+        {
+            var query = db.Tbl_Ogr.OrderBy(x => x.Ogr_Id).Take(3);
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        public void GectiKaldi3()
+        {
+            var query = db.Tbl_Notlar.SelectMany(q => db.Tbl_Ogr.Where(y => y.Ogr_Id == q.OgrId),
+                (x, y) => new
+                {
+                    Ad = y.Ogr_Ad,
+                    Ortalama = x.Ortalama
+                });
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        #region GeçtiKaldı()
+        private void GectiKaldi()
+        {
+            var query = from x1 in db.Tbl_Notlar
+                        join x2 in db.Tbl_Ogr
+                        on x1.OgrId equals x2.Ogr_Id
+                        select new
+                        {
+                            ÖğrenciID = x1.OgrId,
+                            AD = x2.Ogr_Ad,
+                            SOYAD = x2.Ogr_Soyad,
+                            ORTALAMA = x1.Ortalama,
+                            DURUM = x1.Durum == true ? "Geçti" : "Kaldı"
+                        };
+            dataGridView1.DataSource = query.ToList();
+        }
+        #endregion;
+        public void AdBuyukSoyadKucukSartliSecim()
+        {
+            var query = db.Tbl_Ogr.Select(q => new //Anonymous
+            {
+                Adı = q.Ogr_Ad.ToUpper(),
+                Soyadı = q.Ogr_Soyad.ToLower()
+            }).Where(q => q.Adı != "Elif");
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        private void OgrenciSoyadiGetir()
+        {
+            var query = db.Tbl_Ogr.Select(q => q.Ogr_Soyad);
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        private void AdiSoyadiTexttenAl()
+        {
+            var adiSoyadi = txtAdSoyad.Text;
+
+            var query = db.Tbl_Ogr.Where(q => q.Ogr_Ad == adiSoyadi || q.Ogr_Soyad == adiSoyadi);
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        public void AdiElifOlanlariGetir()
+        {
+            var isim = "Elif";
+            var query = db.Tbl_Ogr.Where(q => q.Ogr_Ad.Contains(isim));
+            //var query2 = db.Tbl_Ogr.Where(q => q.Ogr_Ad=="Elif"); // de olur!
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        public void Checked50denKucuk()
+        {
+            var degerler = from p1 in db.Tbl_Notlar
+                           where (p1.Sinav1 < 50)
+                           join p2 in db.Tbl_Ogr
+                           on p1.OgrId equals p2.Ogr_Id
+                           select new
+                           {
+
+                               ADI = p2.Ogr_Ad,
+                               SOYADI = p2.Ogr_Soyad,
+                               NOT = p1.Sinav1
+                           };
+            dataGridView1.DataSource = degerler.ToList();
+        }
+    }
+}
